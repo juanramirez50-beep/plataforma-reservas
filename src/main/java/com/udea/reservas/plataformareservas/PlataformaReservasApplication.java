585b204dc64244315f0e8c3b1ffb96e2eaeece6e
maria <mariaortizseg@gmail.com>
2026-09-09 19:53:35 -0500

    Proyecto base Spring Boot - configuración inicial

---
+++ src/main/java/com/udea/reservas/plataformareservas/PlataformaReservasApplication.java
@@ -0,0 +1,13 @@
+package com.udea.reservas.plataformareservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaReservasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaReservasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udea/reservas/plataformareservas/PlataformaReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.reservas.plataformareservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaReservasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
